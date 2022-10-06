48cb4fdc7977169b01018f787771fb573325263e
hatso <mmorpg3636@naver.com>
2022-10-06 10:31:55 +0900

    10/05 node

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
 };
-//hercku 배포 사이트에서 사용 MONGO_URI 변수를 사이트에서 관리해줌
+//heroku 배포 사이트에서 사용 MONGO_URI 변수를 사이트에서 관리해줌
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +33,46 @@ const userSchema = mongoose.Schema({
     type: Number,
   },
 });
+
+//mongoDB에서 제공하는 기능 pre('save', function(){}) save하기 전에 function(){} 하겠다.
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  if (user.isModified("password")) {
+    //비밀번호를 암호화 시킨다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 1234567 암호화된 비밀번호 $32r32lkfsd23423k413r413@234k  복호화가 불가능해 입력받은 패스워드를 암호화해 비교
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+
+  // jsonwebtoken을 이용해서 token을 생성하기
+  var token = jwt.sign(user._id, "secretToken");
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
